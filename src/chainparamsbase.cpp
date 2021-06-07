2c2fdcbf17c2f99d2c2911029e4b2365ce55487d
minblock <sev@provgn.com>
2021-06-07 13:32:19 +0000

    rpc ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -36,7 +36,7 @@ std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain
     if (chain == CBaseChainParams::MAIN)
         return MakeUnique<CBaseChainParams>("", 13840);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet4", 19332);
+        return MakeUnique<CBaseChainParams>("testnet4", 113840);
     else if (chain == CBaseChainParams::REGTEST)
         return MakeUnique<CBaseChainParams>("regtest", 19443);
     else
