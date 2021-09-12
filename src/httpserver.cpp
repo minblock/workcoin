1f66b30c0fc816f3ebac33ecf944f0e41b8d155f
stevecat <38344452+minblock@users.noreply.github.com>
2021-09-12 14:32:00 -0400

    Update httpserver.cpp
    U already know dont watch this. Itis for a crosscompilation attempt

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "httpserver.h"
-
+#include <deque>
 #include "chainparamsbase.h"
 #include "compat.h"
 #include "util.h"
